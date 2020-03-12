f15e5a6d498b7ab32354caf905b82bf5c205ae78
lideheng <lideheng6379@163.com>
2020-03-12 20:48:17 +0800

    修改

--- demo/src/main/java/com/ldh/DemoApplication.java
+++ demo/src/main/java/com/ldh/DemoApplication.java
@@ -3,15 +3,22 @@ package com.ldh;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @MapperScan(basePackages = { "com.ldh.**.dao" })
 @SpringBootApplication
 @EnableSwagger2
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
 	}
 
 }
